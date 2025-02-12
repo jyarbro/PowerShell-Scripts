53233340dec494235c13c956fa503c00df9bf64e
Jim Yarbro <yarbro@outlook.com>
2025-02-12 12:11:00 -0500

    add all functions to the environment

--- Microsoft.VSCode_profile.ps1
+++ Microsoft.VSCode_profile.ps1
@@ -1,1 +1,6 @@
-. "$PSScriptRoot\my-scripts\Find-LongPaths.ps1"
+Set-Location -Path $HOME
+
+$scriptDir = "$PSScriptRoot\my-scripts"
+Get-ChildItem -Path $scriptDir -Filter *.ps1 | ForEach-Object {
+    . $_.FullName
+}
