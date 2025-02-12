3c5c75e9147b82a50e0e0f014cbbe849b6662a1b
Jim Yarbro <yarbro@outlook.com>
2025-02-12 12:42:38 -0500

    going to handle this differently

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,3 @@ $scriptDir = "$PSScriptRoot\my-scripts"
 Get-ChildItem -Path $scriptDir -Filter *.ps1 | ForEach-Object {
     . $_.FullName
 }
-
-$global:YT_DLP_PATH   = "$env:LOCALAPPDATA\bin\yt-dlp\"
-$global:YT_OUTPUT_DIR = Get-DownloadsPath
